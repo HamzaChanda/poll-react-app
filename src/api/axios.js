441a8eef4ffbc720e2c3a9a484c6f4fff0324423
HamzaChanda <hchanda2003@gmail.com>
2025-08-21 19:41:47 +0530

    added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,14 @@
 // src/api/axios.js
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://poll-app-backend.onrender.com/api"
+    : "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true, // ✅ needed if using cookies
+  withCredentials: true, // needed if using cookies
 });
 
 export default api;
